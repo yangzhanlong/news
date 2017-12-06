8aa23d29c192ea6711d8237af68bfb92de8cd4c0
Zhanlong Yang <673029686@qq.com>
2017-12-06 21:10:09 +0800

    Add Share SDK for share

--- zhbj/src/main/java/org/me/zhbj/activity/NewsDetailActivity.java
+++ zhbj/src/main/java/org/me/zhbj/activity/NewsDetailActivity.java
@@ -30,6 +30,7 @@ import org.me.zhbj.uttils.MyLogger;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 
 public class NewsDetailActivity extends AppCompatActivity {
 
@@ -212,6 +213,7 @@ public class NewsDetailActivity extends AppCompatActivity {
                 changeWebViewTextSize();
                 break;
             case R.id.ib_share:
+                showShare();
                 break;
         }
     }
@@ -253,5 +255,34 @@ public class NewsDetailActivity extends AppCompatActivity {
                 })
                 .setNegativeButton("取消", null)
                 .show();
+    }
+
+
+    private void showShare() {
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+        // 分享时Notification的图标和文字  2.5.9以后的版本不     调用此方法
+        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+        oks.setTitle("我的分享");
+        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+        oks.setTitleUrl("http://sharesdk.cn");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我是分享文本");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        oks.setImagePath("http://images.tmtpost.com/uploads/images/2017/12/20171205230611160.JPG");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite(getString(R.string.app_name));
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://sharesdk.cn");
+
+        // 启动分享GUI
+        oks.show(this);
     }
 }
